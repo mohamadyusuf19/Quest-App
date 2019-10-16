95af8682037018272a3af6048d1ca45346ff664e
Ahmad <yusufadinata19@gmail.com>
2019-10-16 08:11:49 +0700

    first initial pwa

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,33 @@
 import React from "react";
 import "./home.scss";
 import Button from "../button/Button";
+const images = require("../../assets/learning.png");
 
 const Home = ({ onClickStart }) => {
   return (
     <div className="card-home">
-      <p className="title">Selamat Datang</p>
-      <p className="content">
-        Silahkan berdoa sebelum memulai mengerjakan soal evaluasi. Aturan
-        mengerjakan :
-      </p>
-      <ul>
-        <li>
-          Waktu akan dihitung ketika anda memulai dengan menekan tombol mulai.
-        </li>
-        <li>Jawaban yang benar akan di nilai 1,</li>
-        <li>Jawaban yang salah akan di nilai 0.</li>
-        <li>Gunakan kejujuran dalam mengerjakan evaluasi ini.</li>
-        <li>
-          Catatan : untuk menyimpan setiap jawaban silahkan tekan tombol hijau.
-        </li>
-      </ul>
+      {/* <p className="title">Selamat Datang</p> */}
+      <div className="wrapper-home">
+        <div className="wrapper-images">
+          <img src={images} className="images" alt="images" />
+        </div>
+        <div>
+          <p className="content">Aturan mengerjakan :</p>
+          <ul>
+            <li>
+              Waktu akan dihitung ketika anda memulai dengan menekan tombol
+              mulai.
+            </li>
+            <li>Jawaban yang benar akan di nilai 1,</li>
+            <li>Jawaban yang salah akan di nilai 0.</li>
+            <li>Gunakan kejujuran dalam mengerjakan evaluasi ini.</li>
+            <li>
+              Catatan : untuk menyimpan setiap jawaban silahkan tekan tombol
+              hijau.
+            </li>
+          </ul>
+        </div>
+      </div>
       <Button to="/1" buttonText="Mulai" onClickSoal={onClickStart} />
     </div>
   );
