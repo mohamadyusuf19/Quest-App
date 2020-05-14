cb2e055f2160f3c648cd338b4898e1fe13842343
afazzdev <afaizi1999@gmail.com>
2020-05-14 21:50:09 +0700

    Add review button

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,46 +1,58 @@
-import React from "react";
-import "./result.scss";
-import moment from "moment";
+import React from 'react';
+import './result.scss';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
-moment.locale("id", {
-  months: "Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_November_Desember".split(
-    "_"
+moment.locale('id', {
+  months: 'Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_November_Desember'.split(
+    '_'
   ),
-  weekdays: "Ahad_Senin_Selasa_Rabu_Kamis_Jum'at_Sabtu".split("_")
+  weekdays: "Ahad_Senin_Selasa_Rabu_Kamis_Jum'at_Sabtu".split('_'),
 });
-const Result = ({ nilai, benar, salah, startTime, endTime, kosong }) => {
+const Result = ({
+  nilai,
+  benar,
+  salah,
+  startTime,
+  endTime,
+  kosong,
+  reviewSoal,
+}) => {
   const duration = moment.duration(moment(endTime).diff(moment(startTime)));
   return (
-    <div className="card-result">
-      <div className="flex-result">
-        <p className="text-result">Hari : {moment(startTime).format("dddd")}</p>
-        <p className="text-result">
-          Tanggal : {moment(startTime).format("Do MMMM YYYY")}
+    <div className='card-result'>
+      <div className='flex-result'>
+        <p className='text-result'>Hari : {moment(startTime).format('dddd')}</p>
+        <p className='text-result'>
+          Tanggal : {moment(startTime).format('Do MMMM YYYY')}
         </p>
       </div>
-      <div className="flex-result">
+      <div className='flex-result'>
         <div>
-          <p className="text-result">
-            Mulai : pukul {moment(startTime).format("HH:mm:ss")} WIB
+          <p className='text-result'>
+            Mulai : pukul {moment(startTime).format('HH:mm:ss')} WIB
           </p>
-          <p className="text-result">
-            Selesai : pukul {moment(endTime).format(" HH:mm:ss")} WIB
+          <p className='text-result'>
+            Selesai : pukul {moment(endTime).format(' HH:mm:ss')} WIB
           </p>
-          <p className="text-result">
-            Durasi : {duration.hours()} jam {duration.minutes()} menit{" "}
+          <p className='text-result'>
+            Durasi : {duration.hours()} jam {duration.minutes()} menit{' '}
             {duration.seconds()} detik
           </p>
-          <p className="text-result">Benar : {benar}</p>
-          <p className="text-result">Salah : {salah}</p>
-          <p className="text-result">Kosong : {kosong}</p>
+          <p className='text-result'>Benar : {benar}</p>
+          <p className='text-result'>Salah : {salah}</p>
+          <p className='text-result'>Kosong : {kosong}</p>
         </div>
-        <div className="wrapper-nilai">
-          <p className="value-result">Nilai</p>
-          <p className="nilai">{nilai}</p>
+        <div className='wrapper-nilai'>
+          <p className='value-result'>Nilai</p>
+          <p className='nilai'>{nilai}</p>
+          <span className='review' onClick={reviewSoal}>
+            <Link to='/1'>See the result</Link>
+          </span>
         </div>
       </div>
-      <p className={`text-result ${nilai < 70 ? "red" : "green"}`}>
-        Keterangan : {nilai < 70 ? "Tidak Lulus" : "Lulus"}
+      <p className={`text-result ${nilai < 70 ? 'red' : 'green'}`}>
+        Keterangan : {nilai < 70 ? 'Tidak Lulus' : 'Lulus'}
       </p>
     </div>
   );
